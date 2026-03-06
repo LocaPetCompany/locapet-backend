71b5868853520872d000b62d283647c132932bd8
jinung-vivire <jinung.dev@gmail.com>
2026-03-06 18:48:39 +0900

    chore(infra): Terraform IaC 초기 구성 설정
    - AWS 프로바이더 설정 (ap-northeast-2, Terraform >= 1.6.0)
    - S3 백엔드 + DynamoDB 상태 잠금 설정
    - ECR 리포지토리 정의 (app-api, image scanning)

---
+++ infra/terraform/environments/nonprod/main.tf
@@ -0,0 +1,15 @@
+resource "aws_ecr_repository" "app_api" {
+  name = "locapet/app-api"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Project     = "locapet"
+    Environment = "nonprod"
+    Service     = "app-api"
+    ManagedBy   = "terraform"
+  }
+}
---
+++ infra/terraform/environments/nonprod/output.tf
@@ -0,0 +1,3 @@
+output "app_api_repository_url" {
+  value = aws_ecr_repository.app_api.repository_url
+}
